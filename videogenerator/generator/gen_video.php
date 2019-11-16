bbdb9392087986c5f3da6bce1a0d858f5ffa3858
Wang Da <wangda8791@gmail.com>
2019-11-16 17:10:15 +1000

    reformatting change temp folder

--- videogenerator/generator/gen_video.php
+++ videogenerator/generator/gen_video.php
@@ -2,7 +2,7 @@
 // Backend called in index.php by ajax call
 
 // share.php - functions
-require_once("share.php");
+require_once "share.php";
 
 $image_dir = $_REQUEST['resource'];
 $image_duration = $_REQUEST['duration'];
@@ -12,10 +12,10 @@ $file_name = $_REQUEST['filename'];
 // image_dir: resource image folder name
 // file_name: generating video file name
 // image_duration: duration of one image in seconds
-$result = generateVideo($image_dir, $file_name, $image_duration+0);
+$result = generateVideo($image_dir, $file_name, $image_duration + 0);
 
 if ($result === true) {
-	echo json_encode(array('success'=>true, 'filename'=>$file_name));
+    echo json_encode(array('success' => true, 'filename' => $file_name));
 } else {
-	echo json_encode(array('success'=>false, 'message'=>$result));
+    echo json_encode(array('success' => false, 'message' => $result));
 }
